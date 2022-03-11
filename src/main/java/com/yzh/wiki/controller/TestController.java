85814fc0d1f2d40102bd890404bb68e60c5e4368
yangzihao <835627827@qq.com>
2022-03-11 20:13:21 +0800

    feat:配置端口可以是yml也可以是application.properties,如果是spring Cloud下面的spring Boot 可以通过bootstrap运用nacos来动态配置

--- src/main/java/com/yzh/wiki/controller/TestController.java
+++ src/main/java/com/yzh/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.yzh.wiki.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,9 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello(){
         return "hello world";
+    }
+    @PostMapping("/yzh")
+    public String yzh(String name){
+        return "hello world"+name;
     }
 }
